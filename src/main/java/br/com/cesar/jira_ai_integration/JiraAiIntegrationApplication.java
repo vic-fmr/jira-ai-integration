4b6a77d2942984cd8a17ec779d085d3d94123f38
vic-fmr <victorfmarques12@gmail.com>
2025-09-27 10:58:33 -0300

    feat: back setup

--- src/main/java/br/com/cesar/jira_ai_integration/JiraAiIntegrationApplication.java
+++ src/main/java/br/com/cesar/jira_ai_integration/JiraAiIntegrationApplication.java
@@ -3,6 +3,8 @@ package br.com.cesar.jira_ai_integration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class JiraAiIntegrationApplication {
 
